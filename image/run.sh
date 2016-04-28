4ce04dc3db123884e7eb38b79d3bda7d26161fe0
Julien Levesy <jlevesy@gmail.com>
2016-04-28 21:12:19 +0200

    managed to get the node up

--- image/run.sh
+++ image/run.sh
@@ -1,3 +1,9 @@
 #! /bin/sh
+
+for path in /data/logs /data/log /data/work; do
+  mkdir -p ${path}
+  chown -R elasticsearch:elasticsearch /data
+done
+
 /usr/bin/envtmpl -i ${ES_HOME}/config/elasticsearch.yml.tmpl -o ${ES_HOME}/config/elasticsearch.yml
-${ES_HOME}/bin/elasticsearch
+exec su-exec elasticsearch ${ES_HOME}/bin/elasticsearch
