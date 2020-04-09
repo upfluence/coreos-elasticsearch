ca5ac6610288ae84c7129e112660b74c46eccfe2
tgallice <tmgallice@gmail.com>
2020-04-09 18:27:25 +0200

    es:7.2: Remove the initial aws credential setup

--- image/7.2/run.sh
+++ image/7.2/run.sh
@@ -1,7 +1,4 @@
 #! /bin/sh
 /usr/bin/envtmpl -i /usr/share/elasticsearch/config/elasticsearch.yml.tmpl -o /usr/share/elasticsearch/config/elasticsearch.yml
 
-echo $AWS_ACCESS_KEY | /usr/share/elasticsearch/bin/elasticsearch-keystore add --stdin s3.client.default.access_key
-echo $AWS_SECRET_KEY | /usr/share/elasticsearch/bin/elasticsearch-keystore add --stdin s3.client.default.secret_key
-
 /usr/local/bin/docker-entrypoint.sh
